7d709591075cc386d137d08b5361bec4b0cddaea
AsiaWu_a <wuasia2007@hotmail.com>
2025-11-05 21:24:01 +0800

    step eight

---
+++ factorial/factorial/factorial.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+double factorial(int n)
+{
+	int res = 1;
+	for (int i = 1; i <= n; i++)
+	{
+		res *= i;
+	}
+	return res;
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("%lf\n", factorial(n));
+	return 0;
+}
