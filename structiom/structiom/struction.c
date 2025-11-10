e9294cca09ba83e544bc578082eeb7b257dca6fe
AsiaWu_a <wuasia2007@hotmail.com>
2025-11-10 22:38:38 +0800

    step twenty-seven

---
+++ structiom/structiom/struction.c
@@ -0,0 +1,23 @@
+//#include<stdio.h>
+//struct stu
+//{
+//	//³ÉÔ±
+//	char name[20];
+//	int age;
+//	char sex[10];
+//	char tele[12];
+//};
+//void print(struct stu *p)
+//{
+//	printf("%s %d %s %s\n", (*p).name, (*p).age, (*p).sex, (*p).tele);
+//	printf("%s %d %s %c\n", p->name, p->age, p->sex, p->tele);
+//}
+//int main()
+//{
+//
+//	struct stu s = { "zhangsan",20,"man","15596668862" };
+//	printf("%s %d %s %c\n", s.name, s.age, s.sex, s.tele);
+//	return 0;
+//}
+
+ 
