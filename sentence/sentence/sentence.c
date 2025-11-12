877b6a732a409f24b4e3c18f2160bb6aa9fb6671
AsiaWu_a <wuasia2007@hotmail.com>
2025-11-12 10:10:06 +0800

    step twenty-eight

---
+++ sentence/sentence/sentence.c
@@ -0,0 +1,95 @@
+//#include<stdio.h>
+//int test()
+//{
+//	int a = 4;
+//	if (a == 3)
+//	{
+//		return 1;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	/*int a = 2;
+//	if (a>=10)
+//	{
+//		printf("hello");
+//	}
+//	else
+//	{
+//		printf("wocaonima");
+//	}*/
+//
+//	int age = 10;
+//	if (age < 18)
+//	{
+//		printf("teenage");
+//		int a = 10;
+//		int n = 20;
+//	}	int b = 10;
+//	//代码老司机
+//	//公司中的人是会被迭代的//我其实是想的啊动词投入的怎么样啊？
+//	return 0;
+//}
+
+#include<stdio.h>
+
+int main()
+{
+	/*int num = 3;
+	if (num == 5)
+	{
+		printf("he he\n");
+	}*/
+	//int num = 4;
+	///*scanf("%d", &num);*/
+	//if (num % 2 == 0)
+	//{
+	//	printf("是偶数。");
+	//}
+	//else
+	//{
+	//	printf("是奇数。");
+	//}
+
+
+	/*int day;
+	scanf_s("%d", &day);
+	switch (day)
+	{
+	case 1:
+		printf("星期一");
+		break;
+	case 2:
+		printf("星期二");
+		break;
+	case 3:
+		printf("星期三");
+		break;
+	case 4:
+		printf("星期四");
+		break;
+	case 5:
+		printf("星期五");
+		break;
+	case 6:
+		printf("星期六");
+		break;
+	case 7:
+		printf("星期日");
+		break;
+	default:
+		printf("不是星期数，请重新输入：");
+		break;
+	}*/
+	char charactor = 'a';
+	switch (charactor)
+	{
+	case 'a':
+		printf("wcoca ");
+	}
+	
+
+	//print(hello world);
+	return 0;
+}
