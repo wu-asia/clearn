7ca689c1dcceb568b7fd569b4887da23b25dafcc
AsiaWu_a <wuasia2007@hotmail.com>
2025-11-29 19:28:19 +0800

    step seventy-seven

---
+++ improve_chess/improve_chess/game.c
@@ -0,0 +1,191 @@
+#include"game.h"
+
+void IntiBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+
+void DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j != col - 1)
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+		for (j = 0; j < col; j++)
+		{
+			if (i != row - 1)
+			{
+				printf("---");
+				if (j != col - 1)
+				{
+					printf("|");
+				}
+			}
+		}
+		printf("\n");
+	}
+}
+
+void PlayerMove(char arr[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("玩家下棋:>\n");
+	while (1)
+	{
+		printf("请输入坐标:>");
+		scanf("%d %d", &x, &y);
+		//坐标范围合法判断
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (arr[x - 1][y - 1] == ' ')
+			{
+				arr[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("坐标被占用，不能下棋\n");
+			}
+		}
+		else
+		{
+			printf("坐标非法，请重新输入！");
+		}
+	}
+}
+
+void ComputerMove(char arr[ROW][COL], int row, int col)
+{
+	printf("电脑下棋:>\n");
+
+	int x = 0;
+	int y = 0;
+	while (1)
+	{
+		x = rand() % row;
+		y = rand() % col;
+		if (arr[x][y] == ' ')
+		{
+			arr[x][y] = '#';
+			break;
+		}
+	}
+
+}
+
+void PlayAMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	while (1)
+	{
+		printf("请输入坐标:>");
+		scanf("%d %d", &x, &y);
+		if (x >= 1 && x <= ROW && y >= 1 && y <= COL)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+		}
+		else
+		{
+			printf("输入错误，请重新输入！\n");
+		}
+	}
+}
+
+void PlayBMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	while (1)
+	{
+		printf("请输入坐标:>");
+		scanf("%d %d", &x, &y);
+		if (x >= 1 && x <= ROW && y >= 1 && y <= COL)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '#';
+				break;
+			}
+		}
+		else
+		{
+			printf("输入错误，请重新输入！\n");
+		}
+	}
+}
+
+int IsFull(char board[ROW][COL], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;
+			}
+		}
+	}
+	return 1;
+}
+
+char IsWin(char board[ROW][COL], int row, int col)
+{
+	//行
+	int i = 0;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][1] != ' ')
+		{
+			return board[i][1];
+		}
+	}
+	//列
+	int j = 0;
+	for (j = 0; j < row; j++)
+	{
+		if (board[0][j] == board[1][j] && board[1][j] == board[2][j] && board[1][j] != ' ')
+		{
+			return board[1][j];
+		}
+	}
+	//对角线
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
+	{
+		return board[1][1];
+	}
+	if (IsFull(board, ROW, COL))
+	{
+		return 'Q';
+	}
+	return 'C';
+}
